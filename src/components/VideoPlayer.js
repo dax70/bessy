c27b09067a261f40f32b841d5691f5e6df0b5a95
Eddy Recio <eddy@ngensoft.net>
2017-02-21 00:55:52 -0800

    Fix: video now plays and resets

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
-
-// const playerState = {
-//   stop: 'stop',
-//   start: 'start',
-//   pause: 'pause'
-// };
+import classNames from 'classnames';
 
 export default class VideoPlayer extends Component {
 
   constructor(props) {
       super(props);
       this.onClick = this.onClick.bind(this);
+      this.state = { started: false };
+  }
+
+  videLoad = ()=> {
+    this.setState(prevState => ({
+      started: !prevState.started
+    }));
+
+    this.refs.video.load();
+  };
+
+  componentDidMount() {
+    const video = this.refs.video;
+    video.addEventListener('ended', this.videLoad);
+  }
+
+  componentWillUnMount() {
+    const video = this.refs.video;
+    video.removeEventListener('ended', this.videLoad);
   }
 
   onClick() {
-    // @TODO: fix video player
+    this.setState(prevState => ({
+      started: !prevState.started
+    }));
+
+    const video = this.refs.video;
+    video.currentTime = 0;
+    video.play();
   }
 
   render() {
+    const buttonStyles = classNames(
+      'action action--play action--faded',{
+      'action--shown': !this.state.started
+    });
+
     return (
       <div className="video">
-        <video className="video-player"
+        <video ref="video" className="video-player"
                 src="media/sintel.mp4" preload="auto" poster="media/sintel.jpg">
           <source src="media/sintel.ogg"
                   type="video/ogg; codecs=&quot;theora, vorbis&quot;" />
@@ -28,7 +53,7 @@ export default class VideoPlayer extends Component {
                   type="video/mp4; codecs=&quot;avc1.4D401E, mp4a.40.2&quot;" />
           <p>Sorry, your browser does not support this video format.</p>
         </video>
-        <button className="action action--play action--shown action--faded"
+        <button className={buttonStyles}
                 aria-label="Play Video" onClick={ this.onClick }>
         </button>
       </div>
